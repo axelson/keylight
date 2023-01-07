5b934a0d71b02421832487d3255206518e0ca744
Jason Axelson <jason.axelson@gmail.com>
2023-01-07 11:46:57 -1000

    Add caching and formatting

--- lib/keylight.ex
+++ lib/keylight.ex
@@ -3,14 +3,20 @@ defmodule Keylight do
   Documentation for `Keylight`.
   """
 
+  require Logger
   require MdnsLite.DNS
 
   @query_devices MdnsLite.DNS.dns_query(class: :in, type: :ptr, domain: '_elg._tcp.local')
   @default_timeout 2000
 
+  defmodule Device do
+    defstruct [:host, :name, :port, :ip]
+  end
+
   def discover(timeout \\ @default_timeout) do
     query_mdns()
     :timer.sleep(timeout)
+
     case check_mdns() do
       %{additional: []} -> %{}
       %{additional: records} -> records_to_devices(records)
@@ -21,17 +27,60 @@ defmodule Keylight do
     records
     |> Enum.map(fn record ->
       {:dns_rr, identifier, record_type, _, _, _, contents, _, _, _} = record
+
       case record_type do
-        :srv -> {to_string(identifier), %{host: srv_to_host(contents), port: srv_to_port(contents)}}
-        :txt -> {to_string(identifier), %{name: txt_to_name(contents)}}
+        :srv ->
+          {to_string(identifier), %{host: srv_to_host(contents), port: srv_to_port(contents)}}
+
+        :txt ->
+          {to_string(identifier), %{name: txt_to_name(contents)}}
       end
     end)
     |> Enum.reduce(%{}, fn {key, data}, acc ->
-      d = acc
-          |> Map.get(key, %{})
-          |> Map.merge(data)
+      d =
+        acc
+        |> Map.get(key, %{})
+        |> Map.merge(data)
+
       Map.put(acc, key, d)
     end)
+    |> Enum.flat_map(fn
+      {name, %{host: host} = raw_device} ->
+        case MdnsLite.gethostbyname(host) do
+          {:ok, ip} ->
+            # Check if the IP is actually up
+            case :gen_tcp.connect(ip, 9123, [], 1_500) do
+              {:ok, port} ->
+                # Don't leave the port lingering
+                :gen_tcp.close(port)
+
+                device = %Device{
+                  host: host,
+                  name: raw_device.name,
+                  port: raw_device.port,
+                  ip: :inet.ntoa(ip)
+                }
+
+                [{name, device}]
+
+              error ->
+                Logger.warn("Device does not appear to actually be up #{inspect(error)}")
+                []
+            end
+
+          {:error, error} ->
+            Logger.warn("Unable to lookup mdns hostname due to error: #{inspect(error)}")
+            []
+        end
+
+      {name, raw_device} ->
+        Logger.warn(
+          "Unable to instantiate a device for #{inspect(name)} from #{inspect(raw_device)}"
+        )
+
+        []
+    end)
+    |> Map.new()
   end
 
   def info(%{host: _} = device) do
@@ -68,19 +117,21 @@ defmodule Keylight do
 
   @options [:on, :brightness, :temperature]
   def set(%{host: _} = device, opts) do
-    data = opts
-          |> Enum.map(fn {key, value} ->
-            if key in @options do
-              if is_integer(value) do
-                {to_string(key), value}
-              else
-                raise "Bad value #{value} for option '#{key}', should be an integer"
-              end
-            else
-              raise "Bad option '#{key}'"
-            end
-          end)
-          |> Map.new()
+    data =
+      opts
+      |> Enum.map(fn {key, value} ->
+        if key in @options do
+          if is_integer(value) do
+            {to_string(key), value}
+          else
+            raise "Bad value #{value} for option '#{key}', should be an integer"
+          end
+        else
+          raise "Bad option '#{key}'"
+        end
+      end)
+      |> Map.new()
+
     put(device, "elgato/lights", %{"numberOfLights" => 1, "lights" => [data]})
   end
 
@@ -91,7 +142,7 @@ defmodule Keylight do
   end
 
   defp build_url(device, path) do
-    to_charlist("http://#{device.host}:#{device.port}/#{path}")
+    to_charlist("http://#{device.ip}:#{device.port}/#{path}")
   end
 
   defp multi(devices, fun) do
@@ -105,8 +156,14 @@ defmodule Keylight do
   defp get(device, path) do
     :inets.start()
     url = build_url(device, path)
-    {:ok, {_, _, body}} = :httpc.request(:get, {url, []}, [], [])
-    Jason.decode(to_string(body))
+
+    case :httpc.request(:get, {url, []}, [timeout: 1_000, connect_timeout: 1_000], []) do
+      {:ok, {_, _, body}} ->
+        Jason.decode(to_string(body))
+
+      {:error, error} ->
+        {:error, error}
+    end
   end
 
   defp put(device, path, data) do
@@ -126,7 +183,9 @@ defmodule Keylight do
       String.starts_with?(to_string(p), "md=")
     end)
     |> case do
-      nil -> "Unnamed"
+      nil ->
+        "Unnamed"
+
       name ->
         "md=" <> name = to_string(name)
         name
